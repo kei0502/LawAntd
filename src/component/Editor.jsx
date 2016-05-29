77241abee4375e190495cc96b7e36a97f718461a
kei0502 <kei0502@qq.com>
2016-05-29 18:56:49 +0800

    update for react-quill css

--- src/component/Editor.jsx
+++ src/component/Editor.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 var ReactQuill = require('react-quill');
 import { Modal } from 'antd';
-require('../../node_modules/react-quill/node_modules/quill/dist/quill.snow.css');
+require('../css/quill.snow.css');
 var defaultColors = [
   'rgb(  0,   0,   0)', 'rgb(230,   0,   0)', 'rgb(255, 153,   0)',
   'rgb(255, 255,   0)', 'rgb(  0, 138,   0)', 'rgb(  0, 102, 204)',
